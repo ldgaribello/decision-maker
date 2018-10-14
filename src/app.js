a900c350a86e84d4ad3c5b9ae0f51a34705fa9fc
Leon Garibello <lgaribelo@hotmail.com>
2018-10-14 12:38:33 -0500

    Add validator, react and react-dom modules

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import isSenior, { isAdult, canDrink } from "./person.js";
 
+import React from "react";
+import ReactDOM from "react-dom";
+
 console.log("app.js is running...");
 
 console.log("Is an adult: ", isAdult(19));
 console.log("Can drink: ", canDrink(19));
-console.log("Is senior: ", isSenior(66))
+console.log("Is senior: ", isSenior(66));
+
+const template = React.createElement("p", {}, "React test");
+ReactDOM.render(template, document.getElementById("app"));
