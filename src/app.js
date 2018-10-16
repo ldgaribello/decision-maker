655f05fd1ed2bf4f2e6472864112c5d98c0354a6
Leon Garibello <lgaribelo@hotmail.com>
2018-10-15 20:01:31 -0500

    Add babel-loader to webpack config

--- src/app.js
+++ src/app.js
@@ -9,5 +9,5 @@ console.log("Is an adult: ", isAdult(19));
 console.log("Can drink: ", canDrink(19));
 console.log("Is senior: ", isSenior(66));
 
-const template = React.createElement("p", {}, "React test");
+const template = <p>jsx test</p>;
 ReactDOM.render(template, document.getElementById("app"));
