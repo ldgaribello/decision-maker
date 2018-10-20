0cda02f609215a8ed932a95ad07237d1c9c19563
Leon Garibello <lgaribelo@hotmail.com>
2018-10-20 15:10:27 -0500

    Add styles for header component

--- src/app.js
+++ src/app.js
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom";
 
 import DecisionMaker from "./components/DecisionMaker";
 
+import "./styles/styles.scss";
+
 ReactDOM.render(<DecisionMaker />, document.getElementById("app"));
