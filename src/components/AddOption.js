698c11af46e25c567839b5105a84f6140939e160
Leon Garibello <lgaribelo@hotmail.com>
2018-10-29 20:21:07 -0500

    Add styles for options widget

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -21,9 +21,11 @@ export default class AddOption extends React.Component {
   render() {
     return (
       <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.handleAddOption}>
-          <input type="text" name="option" />
+        {this.state.error && (
+          <p className="add-option-error-message">{this.state.error}</p>
+        )}
+        <form onSubmit={this.handleAddOption} className="add-option">
+          <input type="text" name="option" className="add-option__input" />
           <button className="button">Add Option</button>
         </form>
       </div>
--- src/components/Options.js
+++ src/components/Options.js
@@ -3,12 +3,23 @@ import Option from "./Option";
 
 const Options = props => (
   <div>
-    <button onClick={props.handleDeleteOptions} className="button button--link">
-      Remove All
-    </button>
-    {props.options.length === 0 && <p>Please add an option to get started!</p>}
-    {props.options.map(option => (
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your options</h3>
+      <button
+        onClick={props.handleDeleteOptions}
+        className="button button--link"
+      >
+        Remove All
+      </button>
+    </div>
+    {props.options.length === 0 && (
+      <p className="widget__empty-message">
+        Please add an option to get started!
+      </p>
+    )}
+    {props.options.map((option, index) => (
       <Option
+        index= {index}
         key={option}
         optionText={option}
         handleDeleteOption={props.handleDeleteOption}
